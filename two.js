344d1b409c162826d7071e226773365359148364
tvps-sreekar <127391946+tvps-sreekar@users.noreply.github.com>
2023-03-09 14:57:06 +0530

    Add files via upload

---
+++ two.js
@@ -0,0 +1,16 @@
+function add(x,y){
+    return x+y;
+};
+function subtract(x,y){
+    return x-y;
+};
+function multiply(x,y){
+    return x*y;
+};
+function divide(x,y){
+    return x/y;
+};
+exports.add=add;
+ exports.sub=subtract;
+ exports.multiply=multiply;
+ exports.divide=divide;
